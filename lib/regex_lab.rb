339693567ef773395cff4545dceff86712b79f52
Rafael Lopez <lpz.raf@gmail.com>
2018-07-13 21:20:26 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,14 @@
 def starts_with_a_vowel?(word)
-
+    if word.scan(/^aeiou/)
+      return true
+    elsif word.scan(/[^aeiou]/)
+      return false
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+     text.scan(/\w+un\zw+ing/)
+      
 end
 
 def words_five_letters_long(text)
